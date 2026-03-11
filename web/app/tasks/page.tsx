5dc40d2190b2f486dfc63c7c8938358b8d813136
Josue <hounhouijosue1@gmail.com>
2026-03-11 18:09:16 +0100

    feat(web): creating status filtering

--- web/app/tasks/page.tsx
+++ web/app/tasks/page.tsx
@@ -4,10 +4,11 @@ import PriorityFilter from "@/src/components/common/PriorityFiltered";
 import { Task } from "@/src/types/task";
 import SearchBar from "@/src/components/ui/SearchBar";
 import TaskActions from "@/src/components/ui/TaskActions";
+import StatusFiltered from "@/src/components/common/StatusFiltered";
 
-export default async function TasksPage({ searchParams }: { searchParams: Promise<{ priority?: string; search?: string }> }) {
-  const { priority, search } = await searchParams;
-  const tasks = await getTasks(priority as any, search);
+export default async function TasksPage({ searchParams }: { searchParams: Promise<{ priority?: string; search?: string; status: string }> }) {
+  const { priority, search, status } = await searchParams;
+  const tasks = await getTasks(priority, search, status);
 
   return (
     <div className="space-y-10">
@@ -20,7 +21,7 @@ export default async function TasksPage({ searchParams }: { searchParams: Promis
             Vos priorités <span className="text-indigo-600">du jour</span>
           </h1>
           <p className="text-slate-500 text-lg max-w-lg">
-            Vous avez <span className="font-semibold text-slate-900">{tasks.length} tâches</span> pour cette priorité.
+            Vous avez <span className="font-semibold text-slate-900">{tasks.length} tâches</span> pour cette recherche.
           </p>
         </div>
       </section>
@@ -31,8 +32,9 @@ export default async function TasksPage({ searchParams }: { searchParams: Promis
             <SearchBar /> 
           </div>
           <div className="h-8 w-px bg-slate-100 hidden md:block" />
-          <div className="flex items-center gap-2 w-full md:w-auto p-1">
+          <div className="flex md:flex-row flex-col items-center gap-2 w-full md:w-auto p-1">
             <PriorityFilter />
+            <StatusFiltered />
             <TaskActions />
           </div>
         </div>
